ee69340e69e339bfad7d93a94b753fa7a0a29903
devmolozi <jeancordeiro.f@gmail.com>
2025-04-21 01:33:20 -0300

    add lombook and modify

--- Backend/src/main/java/fiorentin/dev/alvos/model/Alvos.java
+++ Backend/src/main/java/fiorentin/dev/alvos/model/Alvos.java
@@ -1,8 +1,18 @@
 package fiorentin.dev.alvos.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import java.time.LocalDate;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 
 // entidade no banco de dados
 @CrossOrigin(origins = "http://localhost:5173")
@@ -19,45 +29,5 @@ public class Alvos {
     private String userSocial;
     private LocalDate dateExecute;
 
-    public Alvos(Long id, String name,String userSocial, LocalDate dateExecute) {
-        this.id = id;
-        this.name = name;
-        this.userSocial = userSocial;
-        this.dateExecute = dateExecute;
 
-    }
-    public Alvos() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getUserSocial() {
-        return userSocial;
-    }
-
-    public void setUserSocial(String userSocial) {
-        this.userSocial = userSocial;
-    }
-
-    public LocalDate getDateExecute() {
-        return dateExecute;
-    }
-
-    public void setDateExecute(LocalDate dateExecute) {
-        this.dateExecute = dateExecute;
-    }
 }
